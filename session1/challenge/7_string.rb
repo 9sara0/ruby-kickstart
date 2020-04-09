0110ab044151d199cd2251704bfe41091772ea55
Sarah <Sarah.alalaya@gmail.com>
2020-04-09 14:07:24 +0300

    Solves challenge 1:7

--- session1/challenge/7_string.rb
+++ session1/challenge/7_string.rb
@@ -1,8 +1,15 @@
 # given a string, return the character after every letter "r"
-# 
+#
 # pirates_say_arrrrrrrrr("are you really learning Ruby?") # => "eenu"
 # pirates_say_arrrrrrrrr("Katy Perry is on the radio!")   # => "rya"
 # pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")        # => "arrrrrrrr"
 
 def pirates_say_arrrrrrrrr(string)
+  returned = ""
+  (string.length - 1).times do |index|
+    if string[index] =~ /[Rr]/
+      returned << string[index+1]
+    end
+  end
+   returned
 end
