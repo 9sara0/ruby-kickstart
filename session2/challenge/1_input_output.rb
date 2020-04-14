ef15af7bb668a136271571ec106cb3688abb299f
Sarah <Sarah.alalaya@gmail.com>
2020-04-14 15:14:29 +0300

    Solves challenge 1:2

--- session2/challenge/1_input_output.rb
+++ session2/challenge/1_input_output.rb
@@ -3,10 +3,17 @@
 
 # Write a program that reads in two integers typed on the keybaord
 # and outputs their sum, difference, and product
-# 
+#
 # Standard input will be like "9 2\n" and will expect you to print
 # "11\n7\n18\n" to standard output.
 
 def sum_difference_product
   # your code goes here
+  puts "Please enter two integers"
+  
+  int1, int2 = gets.chomp.map { |num|  num.to_i}
+
+  puts (int1 + int2)
+  puts (int1 - int2)
+  puts (int1 * int2)
 end
