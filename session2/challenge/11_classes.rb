662417447956132f931780eedd724ef72e93d1c6
Sarah <Sarah.alalaya@gmail.com>
2020-09-01 23:21:42 +0300

    Refactors numbers_to_words

--- session2/challenge/11_classes.rb
+++ session2/challenge/11_classes.rb
@@ -36,87 +36,22 @@ class BeerSong
 
   def print_line(i)
     bottle_bottles = i == 1 ? "bottle" : "bottles"
-      puts "#{(numbers_to_words(i)).capitalize} #{bottle_bottles} of beer on the wall,"
-      puts "#{(numbers_to_words(i)).capitalize} #{bottle_bottles} of beer,"
+      puts "#{(numbers_to_words(i))} #{bottle_bottles} of beer on the wall,"
+      puts "#{(numbers_to_words(i))} #{bottle_bottles} of beer,"
       puts "Take one down, pass it around,"
       i -= 1
       bottle_bottles = i == 1 ? "bottle" : "bottles"
-      puts "#{(numbers_to_words(i)).capitalize} #{bottle_bottles} of beer on the wall."
+      puts "#{(numbers_to_words(i))} #{bottle_bottles} of beer on the wall."
   end
 
   def numbers_to_words(num)
-    return single_digit_to_word(num) if is_a_single_digit?(num)
-    return teens_to_words(num) if is_between_10_and_20?(num)
-    return tens_to_words(num) if is_a_multiple_of_10?(num)
-    two_digit_to_word(num)
-  end
-
-
-
-  def single_digit_to_word(num)
-    case num
-    when 0
-      return 'zero'
-    when 1
-      return 'one'
-    when 2
-      return 'two'
-    when 3
-      return 'three'
-    when 4
-      return 'four'
-    when 5
-      return 'five'
-    when 6
-      return 'six'
-    when 7
-      return 'seven'
-    when 8
-      return 'eight'
-    when 9
-      return 'nine'
-    end
-  end
-
-
-  def teens_to_words(num)
-    case num
-    when 11
-      return 'eleven'
-    when 12
-      return 'twelve'
-    when 13
-      return 'thirteen'
-    when 15
-      return 'fifteen'
-    when 18
-      return 'eighteen'
-    when 14, 16, 17, 19
-      return "#{single_digit_to_word(get_ones(num))}teen"
-    end
-  end
-
-  def tens_to_words(num)
-    case num
-    when 10
-      return 'ten'
-    when 20
-      return 'twenty'
-    when 30
-      return 'thirty'
-    when 40
-      return 'forty'
-    when 50
-      return 'fifty'
-    when 80
-      return 'eighty'
-    when 60, 70, 90
-      return "#{single_digit_to_word(get_tens(num))}ty"
-    end
-  end
-
-  def two_digit_to_word(num)
-    return "#{tens_to_words(get_tens(num)*10)}-#{single_digit_to_word(get_ones(num))}"
+    if num >= 0 && num <= 19
+      %w(zero one two three four five six seven eight nine ten eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen)[num]
+    elsif is_a_multiple_of_10?(num)
+      %w(zero ten twenty thirty forty fifty sixty seventy eighty ninety)[num/10]
+    else
+      "#{numbers_to_words(get_tens(num)*10)}-#{numbers_to_words(get_ones(num))}".downcase
+    end.capitalize
   end
 
   def get_ones(num)
@@ -127,31 +62,8 @@ class BeerSong
     @tens = num/10
   end
 
-  def is_a_single_digit?(num)
-    num < 10
-  end
-
-  def is_between_10_and_20?(num)
-    num > 10 && num < 20
-  end
-
   def is_a_multiple_of_10?(num)
     return true if get_ones(num) == 0
     false
   end
 end
-
-
-
-song  = BeerSong.new(31)
-# puts song.bottles
-# puts song.numbers_to_words(song.bottles)
-song.print_song
-
-# song2  = BeerSong.new(90)
-# puts song2.bottles
-# puts song2.numbers_to_words(song2.bottles)
-#
-# song3  = BeerSong.new(30)
-# puts song3.bottles
-# puts song3.numbers_to_words(song3.bottles)
